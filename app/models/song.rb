29ffcd1862a0d94976f13296ea9f3632cf1775bb
Justin Berger Howes <happymelonbox@gmail.com>
2021-11-01 22:42:28 +1100

    complete

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,17 +4,15 @@ class Song < ActiveRecord::Base
   has_many :notes
 
   def artist_name=(name)
-    artist = Artist.find_or_create_by(name: name)
-    self.artist = artist
+    self.artist = Artist.find_or_create_by(name: name)
   end
 
-  def artist_name 
+  def artist_name
     self.artist ? self.artist.name : nil 
   end
 
   def genre_name=(name)
-    genre = Genre.find_or_create_by(name: name)
-    self.genre = genre
+    self.genre = Genre.find_or_create_by(name: name)
   end
 
   def genre_name
